2412144ae558eb3e708217482af0347a3dae5504
slgeekshow <86388269+youtubeslgeekshow@users.noreply.github.com>
2021-09-10 20:44:12 -0700

    git push

--- DewmiBot/modules/helper_funcs/misc.py
+++ DewmiBot/modules/helper_funcs/misc.py
@@ -53,25 +53,25 @@ def paginate_modules(page_n: int, module_dict: Dict, prefix, chat=None) -> List:
     modules[i * 3:(i + 1) * 3] for i in range((len(modules) + 3 - 1) // 3)
     ]
 
-    round_num = len(modules) / 8
+    round_num = len(modules) / 3
     calc = len(modules) - round(round_num)
     if calc == 1:
         pairs.append((modules[-1], ))
     elif calc == 2:
         pairs.append((modules[-1], ))
 
-    max_num_pages = ceil(len(pairs) /8)
+    max_num_pages = ceil(len(pairs) / 10)
     modulo_page = page_n % max_num_pages
 
     # can only have a certain amount of buttons side by side
-    if len(pairs) > 8:
-        pairs = pairs[modulo_page * 8:8 * (modulo_page + 1)] + [
+    if len(pairs) > 10:
+        pairs = pairs[modulo_page * 10:10 * (modulo_page + 1)] + [
             (EqInlineKeyboardButton("Back", callback_data="{}_prev({})".format(prefix, modulo_page)),
-                EqInlineKeyboardButton("Close", callback_data="aboutmanu_back"),
+                EqInlineKeyboardButton("close", callback_data="aboutmanu_"),
              EqInlineKeyboardButton("Next", callback_data="{}_next({})".format(prefix, modulo_page)))]
 
     else:
-        pairs += [[EqInlineKeyboardButton("close", callback_data="aboutmanu_")]]
+        pairs += [[EqInlineKeyboardButton("Close", callback_data="aboutmanu_")]]
 
     return pairs
 
